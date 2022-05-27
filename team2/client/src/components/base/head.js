42103d9eb6842bae53bb4ff491d3b78b52a0a8cc
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-05-26 21:21:38 -0400

    point img in get idea to right location

--- team2/client/src/components/base/head.js
+++ team2/client/src/components/base/head.js
@@ -13,11 +13,11 @@ class Head extends HTMLElement {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="shortcut icon" href="./src/img/jar_favicon.ico">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Cambay:wght@400;700&family=Handlee&display=swap" rel="stylesheet">
             <title>Idea Jar</title>
-            <link rel="shortcut icon" href="./src/img/jar_favicon.ico">
         </head>
         `;
     }
